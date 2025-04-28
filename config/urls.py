2d7a2b63268c7771681576ebbe86c2dda29e31b9
kalms <lozano.ambiente@gmail.com>
2025-04-27 20:15:37 -0500

    Add HTTP HEALTHCHECK to Dockerfile

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from django.http import HttpResponse
 from django.urls import path, include
+from django.http import HttpResponse 
 from django.conf import settings
 from django.conf.urls.static import static
 
